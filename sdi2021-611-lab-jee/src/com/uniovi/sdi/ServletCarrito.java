d9108e05d26a471cadf89695c876c5e42d4ba30c
Elias-Llera <eliaslleragr@gmail.com>
2021-01-31 17:53:59 +0100

    SDI-2021-611-1.2 Manejo de sesiones.”

---
+++ sdi2021-611-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
@@ -0,0 +1,88 @@
+package com.uniovi.sdi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletCarrito
+ */
+@WebServlet("/incluirEnCarrito")
+public class ServletCarrito extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletCarrito() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		HttpSession session = request.getSession();
+
+		HashMap<String, Integer> carrito = (HashMap<String, Integer>) request.getSession().getAttribute("carrito");
+		// No hay carrito, creamos uno y lo insertamos en sesión
+		if (carrito == null) {
+			carrito = new HashMap<String, Integer>();
+			request.getSession().setAttribute("carrito", carrito);
+		}
+		String producto = request.getParameter("producto");
+		if (producto != null) {
+			insertarEnCarrito(carrito, producto);
+		}
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println(carritoEnHTML(carrito) + "<br>");
+		out.println("<a href=\"tienda.html\">Volver</a></BODY></HTML>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	// -----------------------------MÉTODOS AUXILIARES-----------------------------
+	private void insertarEnCarrito(HashMap<String, Integer> carrito, String producto) {
+		if (carrito.get(producto) == null) {
+			carrito.put(producto, 1);
+		} else {
+			int numeroArticulo = carrito.get(producto);
+			carrito.put(producto, ++numeroArticulo);
+		}
+	}
+
+	private String carritoEnHTML(HashMap<String, Integer> carrito) {
+		String carritoEnHTML = "";
+		for (String key : carrito.keySet()) {
+			carritoEnHTML += "<p>[" + key + "], " + carrito.get(key) + " unidades</p>";
+		}
+		return carritoEnHTML;
+	}
+
+}
