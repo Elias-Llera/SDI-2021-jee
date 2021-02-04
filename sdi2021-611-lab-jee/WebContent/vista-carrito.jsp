0d3bff88c2a7c3f6f740e6ea4328801296ee21d4
Elias-Llera <eliaslleragr@gmail.com>
2021-02-04 17:35:21 +0100

    SDI-2021-611-1.8-MVC, Modelo Vista Controlador.

---
+++ sdi2021-611-lab-jee/WebContent/vista-carrito.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+
+<head>
+<title>JSP</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<!-- Contenido -->
+	<div class="container" id="contenedor-principal">
+		<h2>Vista-Carrito</h2>
+		<ul>
+			<c:forEach var="par" items="${paresCarrito}">
+				<tr>
+					<li>${par.key}-${par.value}</li>
+				</tr>
+			</c:forEach>
+		</ul>
+	</div>
+	<a href="">Volver</a>
+</body>
--- sdi2021-611-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
+++ sdi2021-611-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
@@ -47,14 +47,18 @@ public class ServletCarrito extends HttpServlet {
 		if (producto != null) {
 			insertarEnCarrito(carrito, producto);
 		}
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println(carritoEnHTML(carrito) + "<br>");
-		out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
+//		response.setCharacterEncoding("UTF-8");
+//		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+//		out.println("<HTML>");
+//		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
+//		out.println("<BODY>");
+//		out.println(carritoEnHTML(carrito) + "<br>");
+//		out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
+
+		// Retornar la vista con parámetro "carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
 	}
 
 	/**
