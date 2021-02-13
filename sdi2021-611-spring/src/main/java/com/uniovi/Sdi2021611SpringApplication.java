aeb6b3bddcc9a31f1e2eb5f018350ba7a4595faa
Elias-Llera <eliaslleragr@gmail.com>
2021-02-13 17:25:43 +0100

    SDI-2021-611-2.1-Create Spring project.

---
+++ sdi2021-611-spring/src/main/java/com/uniovi/Sdi2021611SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021611SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021611SpringApplication.class, args);
+	}
+
+}
---
+++ sdi2021-611-spring/src/test/java/com/uniovi/Sdi2021611SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2021611SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
