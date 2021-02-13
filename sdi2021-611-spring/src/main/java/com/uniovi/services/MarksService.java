d7287217588ce8c5a6756875435d95f046c0af85
Elias-Llera <eliaslleragr@gmail.com>
2021-02-13 19:23:15 +0100

    SDI-2021-611-2.4-Modelo, acceso a datos simple.

--- sdi2021-611-spring/src/main/java/com/uniovi/services/MarksService.java
+++ sdi2021-611-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -1,41 +1,36 @@
 package com.uniovi.services;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.repositories.MarksRepository;
 
 @Service
 public class MarksService {
-	private List<Mark> marksList = new LinkedList<Mark>();
 
-	@PostConstruct
-	public void init() {
-		marksList.add(new Mark(1L, "Ejercicio 1", 10.0));
-		marksList.add(new Mark(2L, "Ejercicio 2", 9.0));
-	}
+	@Autowired
+	private MarksRepository marksRepository;
 
 	public List<Mark> getMarks() {
-		return marksList;
+		List<Mark> marks = new ArrayList<Mark>();
+		marksRepository.findAll().forEach(marks::add);
+		return marks;
 	}
 
 	public Mark getMark(Long id) {
-		return marksList.stream().filter(mark -> mark.getId().equals(id)).findFirst().get();
+		return marksRepository.findById(id).get();
 	}
 
 	public void addMark(Mark mark) {
-// Si en Id es null le asignamos el ultimo + 1 de la lista
-		if (mark.getId() == null) {
-			mark.setId(marksList.get(marksList.size() - 1).getId() + 1);
-		}
-		marksList.add(mark);
+		// Si en Id es null le asignamos el ultimo + 1 de la lista
+		marksRepository.save(mark);
 	}
 
 	public void deleteMark(Long id) {
-		marksList.removeIf(mark -> mark.getId().equals(id));
+		marksRepository.deleteById(id);
 	}
 }
