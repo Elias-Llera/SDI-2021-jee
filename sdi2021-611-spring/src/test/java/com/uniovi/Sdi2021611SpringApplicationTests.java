d9337fa00a8d8518f57d9c23249fdd04d647c2f1
Elias-Llera <eliaslleragr@gmail.com>
2021-03-06 19:22:34 +0100

    SDI-2021-611-5.1-ClasePrincipal.

--- sdi2021-611-spring/src/test/java/com/uniovi/Sdi2021611SpringApplicationTests.java
+++ sdi2021-611-spring/src/test/java/com/uniovi/Sdi2021611SpringApplicationTests.java
@@ -1,13 +1,56 @@
 package com.uniovi;
 
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class Sdi2021611SpringApplicationTests {
 
+	static String PathFirefox65 = "C:\\Archivos de programa\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver024 = "C:\\Users\\Elías\\Documents\\Universidad\\SDI\\lab\\sesion5\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "http://localhost:8090";
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		driver.navigate().to(URL);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.manage().deleteAllCookies();
+	}
+
+	@BeforeClass
+	static public void begin() {
+	}
+
+	@AfterClass
+	static public void end() {
+		driver.quit();
+	}
+
 	@Test
-	void contextLoads() {
+	public void test() {
+		fail("Not yet implemented");
 	}
 
 }
